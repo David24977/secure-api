01a4afa094d82164ce32c76657605d05f8f8ed86
David <davidferrersapinya@gmail.com>
2025-12-20 13:37:48 +0100

    Initial Spring Boot project with Spring Security

---
+++ src/test/java/com/david/secureApi/SecureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.secureApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
